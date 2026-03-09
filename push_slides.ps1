eb50c9e76b80832772bb137d2160d40b9f6472d5
mun77 <51088127+Ko-Haku@users.noreply.github.com>
2026-03-09 13:25:13 +0100

    Add index.html, fix manifests with correct URLs

---
+++ push_slides.ps1
@@ -0,0 +1,7 @@
+﻿Set-Location "D:\Unity\LabEconomia\LabEconomia_Slides_temp"
+git add -A
+git status
+git commit -m "Add index.html, fix manifests with correct URLs"
+git push origin main
+Write-Host "DONE" -ForegroundColor Green
+
